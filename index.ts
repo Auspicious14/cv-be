6cfecf327f5930d54cb3eadafeb8a64f37130715
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2023-04-28 23:41:58 +0100

    init project

---
+++ index.ts
@@ -0,0 +1,20 @@
+const dotenv = require("dotenv");
+dotenv.config();
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+export const appRoute = express();
+
+appRoute.use(cors());
+appRoute.use((req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow_Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+appRoute.use(express.json({ limit: "50mb" }));
+appRoute.use(express.urlencoded({ limit: "50mb", extended: true }));
+appRoute.use(cookieParser());
+// appRoute.use(express.json());
