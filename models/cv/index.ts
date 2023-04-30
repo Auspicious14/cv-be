ec895bd92fd45d3cc1a2ee76014f855b9e953672
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2023-04-30 14:20:38 +0100

    created cv schema and model

---
+++ models/cv/index.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+const { isEmail } = require("validator");
+
+const Schema = mongoose.Schema;
+
+const CVSchema = new Schema(
+  {
+    personalInformation: {
+      firstName: { type: String, required: true },
+      lastName: { type: String, required: true },
+      email: { type: String, required: true, unique: true, validate: isEmail },
+      dateOfBirth: { type: String },
+      image: {
+        uri: { type: String },
+        name: { type: String },
+        type: { type: String },
+      },
+    },
+    experience: [
+      {
+        jobTitle: { type: String },
+        description: { type: String },
+        company: { type: String },
+        fromDate: { type: String },
+        toDate: { type: String },
+      },
+    ],
+    academic: [
+      {
+        school: { type: String },
+        course: { type: String },
+        fromDate: { type: String },
+        toDate: { type: String },
+      },
+    ],
+    certificate: [{ name: { type: String }, year: { type: String } }],
+    skill: [{ name: String }],
+  },
+  { timestamps: true }
+);
+
+const CVModel = mongoose.model("cv", CVSchema);
+export default CVModel;
