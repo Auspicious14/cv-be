b6f9d880849a27a2de1866af68eecd55177ac674
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2023-04-30 23:41:51 +0100

    modify cv model

--- models/cv/index.ts
+++ models/cv/index.ts
@@ -10,6 +10,12 @@ const CVSchema = new Schema(
       lastName: { type: String, required: true },
       email: { type: String, required: true, unique: true, validate: isEmail },
       dateOfBirth: { type: String },
+      phoneNumber: { type: String },
+      address: { type: String },
+      profession: { type: String },
+      state: { type: String },
+      city: { type: String },
+      country: { type: String },
       image: {
         uri: { type: String },
         name: { type: String },
