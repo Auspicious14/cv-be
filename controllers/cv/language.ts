348f83a5a35316c6fc79236b4de099c5ab5937b2
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2025-04-18 10:01:43 +0100

    [add] language endpoints to CV routes

---
+++ controllers/cv/language.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from "express";
+import CVModel from "../../models/cv";
+import { handleErrors } from "../../middlewares/errorHandler";
+
+export const getLanguage = async (req: Request, res: Response) => {
+  const { cvId } = req.params;
+
+  try {
+    const cv = await CVModel.findOne({ _id: cvId });
+    if (!cv) return res.json({ success: false, message: "CV not found" });
+
+    res.json({ success: true, data: cv.language });
+  } catch (error) {
+    const errors = handleErrors(error);
+    res.json({ success: false, errors });
+  }
+};
+
+export const updateLanguage = async (req: Request, res: Response) => {
+  const { payload } = req.body;
+  const { cvId } = req.params;
+  const userId = (req as any).user?._id;
+
+  try {
+    const updateData = {
+      language: payload,
+      ...(userId
+        ? { userId }
+        : {
+            isGuestCV: true,
+            expiresAt: new Date(Date.now() + 24 * 60 * 60 * 1000),
+          }),
+    };
+    const cv = await CVModel.findOneAndUpdate(
+      userId ? { userId } : { _id: cvId },
+      { $set: updateData },
+      { new: true }
+    );
+
+    if (!userId && !cv?.isGuestCV) {
+      return res.json({ success: false, message: "CV not found" });
+    }
+
+    res.json({ success: true, data: cv?.skill });
+  } catch (error) {
+    const errors = handleErrors(error);
+    res.json({ success: false, errors });
+  }
+};
--- routes/cv.ts
+++ routes/cv.ts
@@ -12,6 +12,7 @@ import {
   getPersonalInfo,
 } from "../controllers/cv/personal";
 import { getSkill, updateSkill } from "../controllers/cv/skill";
+import { getLanguage, updateLanguage } from "../controllers/cv/language";
 
 const cvRoute = express.Router();
 
@@ -26,6 +27,8 @@ cvRoute.put("/cv/:cvId/experience", updateExperience);
 cvRoute.get("/cv/:cvId/experience", getExperience);
 cvRoute.put("/cv/:cvId/skill", updateSkill);
 cvRoute.get("/cv/:cvId/skill", getSkill);
+cvRoute.put("/cv/:cvId/language", updateLanguage);
+cvRoute.get("/cv/:cvId/language", getLanguage);
 cvRoute.get("/cv/:cvId", getCV);
 
 export default cvRoute;
