20f25be327b8021255e42eb51ca54821762b982b
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-04-21 11:18:29 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -21,6 +21,9 @@ console.log("ENV:", process.env.CLIENT_URL)
 appRoute.use(express.json({ limit: "50mb" }));
 appRoute.use(express.urlencoded({ limit: "50mb", extended: true }));
 appRoute.use(cookieParser());
+appRoute.get("/", (req, res) => {
+  res.send("Backend is working!");
+});
 appRoute.use("/auth", router);
 appRoute.use(cvRouter);
 appRoute.use(aiSuggestionRouter);
